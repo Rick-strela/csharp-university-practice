3a5afc100b3400471ab1c32b08548c34be525621
Rick-strela <aziz.miner240@gmail.com>
2026-03-22 10:44:42 +0200

    feat: реалізація алгоритму Grading Students

--- src/hackerrank/Result.cs
+++ src/hackerrank/Result.cs
@@ -1,8 +1,27 @@
-﻿using System;
+﻿using Xunit;
+using System.Collections.Generic;
+using HackerRankTasks;
 
-public class Class1
+public class GradingTests
 {
-	public Class1()
-	{
-	}
+    [Fact]
+    public void TestGradingLogic()
+    {
+        var input = new List<int> { 73, 67, 38, 33 };
+        var expected = new List<int> { 75, 67, 40, 33 };
+
+        var result = Result.gradingStudents(input);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(29, 29)]
+    [InlineData(57, 57)]
+    [InlineData(84, 85)]
+    public void TestIndividualGrades(int inputGrade, int expectedGrade)
+    {
+        var result = Result.gradingStudents(new List<int> { inputGrade });
+        Assert.Equal(expectedGrade, result[0]);
+    }
 }
