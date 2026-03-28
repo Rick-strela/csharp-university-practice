f971aed2c047ce44d7e705923f70a7d4cfc25695
Rick-strela <aziz.miner240@gmail.com>
2026-03-28 14:14:09 +0200

    Completed Apple and Orange task - Practice 4

---
+++ src/hackerrank1/AppleAndOrangeTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using AppleAndOrange;
+
+namespace AppleAndOrange.Tests
+{
+    public class AppleAndOrangeTests
+    {
+        [Fact]
+        public void TestSampleInput()
+        {
+            // Данные из примера на HackerRank
+            int s = 7, t = 11;
+            int a = 5, b = 15;
+            var apples = new List<int> { -2, 2, 1 };
+            var oranges = new List<int> { 5, -6 };
+
+            // Act
+            var (appleResult, orangeResult) = Result.GetCounts(s, t, a, b, apples, oranges);
+
+            // Assert
+            Assert.Equal(1, appleResult);
+            Assert.Equal(1, orangeResult);
+        }
+    }
+}
