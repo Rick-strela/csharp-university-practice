66eb529f6561d0dd856f8899ef672c003323a361
Rick-strela <aziz.miner240@gmail.com>
2026-04-11 21:59:54 +0300

    Add practical task 5: Number Line Jumps solution and xUnit tests

--- src/hackerrank1/AppleAndOrange.cs
+++ src/hackerrank1/AppleAndOrange.cs
@@ -1,8 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
-public class Class1
+namespace AppleAndOrange
 {
-	public Class1()
-	{
-	}
+    public class Result
+    {
+        public static (int, int) GetCounts(int s, int t, int a, int b, List<int> apples, List<int> oranges)
+        {
+            int appleCount = 0;
+            int orangeCount = 0;
+
+            foreach (int apple in apples)
+            {
+                int pos = a + apple;
+                if (pos >= s && pos <= t) appleCount++;
+            }
+
+            foreach (int orange in oranges)
+            {
+                int pos = b + orange;
+                if (pos >= s && pos <= t) orangeCount++;
+            }
+
+            return (appleCount, orangeCount);
+        }
+
+        public static void countApplesAndOranges(int s, int t, int a, int b, List<int> apples, List<int> oranges)
+        {
+            var (appleCount, orangeCount) = GetCounts(s, t, a, b, apples, oranges);
+            Console.WriteLine(appleCount);
+            Console.WriteLine(orangeCount);
+        }
+    }
 }
---
+++ tests/NumberLineJumpsTEST.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using KangarooApp.Hackerrank;
+
+namespace KangarooApp.Tests
+{
+    public class Task3Tests
+    {
+        [Fact]
+        public void TestMeetingPointExists()
+        {
+            string result = Task3.Kangaroo(0, 3, 4, 2);
+
+            Assert.Equal("YES", result);
+        }
+
+        [Fact]
+        public void TestMeetingPointDoesNotExist()
+        {
+            string result = Task3.Kangaroo(0, 2, 5, 3);
+            Assert.Equal("NO", result);
+        }
+
+        [Fact]
+        public void TestSlowerKangarooBehind()
+        {
+            Assert.Equal("NO", Task3.Kangaroo(0, 2, 5, 2));
+            Assert.Equal("NO", Task3.Kangaroo(0, 1, 5, 3));
+        }
+    }
+}
