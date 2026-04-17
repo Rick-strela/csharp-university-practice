1fcf6eeb9f479e9be38f2fe50e06cc041c2b2740
Rick-strela <aziz.miner240@gmail.com>
2026-04-17 16:39:00 +0300

    Practical 5: Task 3 (Between Two Sets) complete with tests

---
+++ src/hackerrank/BetweenTwoSets.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankTasks
+{
+    public class Task3
+    {
+        public static int GetTotalX(List<int> a, List<int> b)
+        {
+            int count = 0;
+
+            int start = a.Max();
+            int end = b.Min();
+            for (int i = start; i <= end; i++)
+            {
+                bool condition1 = true;
+                foreach (int x in a)
+                {
+                    if (i % x != 0)
+                    {
+                        condition1 = false;
+                        break;
+                    }
+                }
+
+                if (condition1)
+                {
+                    bool condition2 = true;
+                    foreach (int x in b)
+                    {
+                        if (x % i != 0)
+                        {
+                            condition2 = false;
+                            break;
+                        }
+                    }
+
+                    if (condition2)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ tests/BetweenTwoSetsTEST.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankTasks
+{
+    public class Task3Tests
+    {
+        public void RunTests()
+        {
+            var a1 = new List<int> { 2, 6 };
+            var b1 = new List<int> { 24, 36 };
+            int result1 = Task3.GetTotalX(a1, b1);
+            Console.WriteLine(result1 == 2 ? "Test 1 Passed" : "Test 1 Failed");
+
+            var a2 = new List<int> { 2, 4 };
+            var b2 = new List<int> { 16, 32, 96 };
+            int result2 = Task3.GetTotalX(a2, b2);
+            Console.WriteLine(result2 == 3 ? "Test 2 Passed" : "Test 2 Failed");
+        }
+    }
+}
