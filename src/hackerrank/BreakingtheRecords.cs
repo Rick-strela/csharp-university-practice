dee5d3e989829b8928f8a2d08ac31c86bd6ca2de
Rick-strela <aziz.miner240@gmail.com>
2026-04-26 16:03:55 +0300

    Практична №7: Рiшення задачи Breaking the Records

---
+++ src/hackerrank/BreakingtheRecords.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Result
+{
+    public static List<int> breakingRecords(List<int> scores)
+    {
+        if (scores == null || scores.Count == 0) return new List<int> { 0, 0 };
+
+        int max = scores[0];
+        int min = scores[0];
+
+        int countMax = 0;
+        int countMin = 0;
+
+        for (int i = 1; i < scores.Count; i++)
+        {
+            if (scores[i] > max)
+            {
+                max = scores[i];
+                countMax++;
+            }
+            else if (scores[i] < min)
+            {
+                min = scores[i];
+                countMin++;
+            }
+        }
+
+        return new List<int> { countMax, countMin };
+    }
+}
