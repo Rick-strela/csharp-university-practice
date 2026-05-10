3d1125e362f5db0ab3a067565b5f4a5e0f6fb416
Rick-strela <aziz.miner240@gmail.com>
2026-05-10 11:13:43 +0300

    feat: implement sock merchant pair counting logic

---
+++ src/hackerrank/sockMerchant.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Solution
+{
+    public static int sockMerchant(int n, List<int> ar)
+    {
+        HashSet<int> lonelySocks = new HashSet<int>();
+        int pairs = 0;
+
+        foreach (int sock in ar)
+        {
+            if (lonelySocks.Contains(sock))
+            {
+                pairs++;
+                lonelySocks.Remove(sock);
+            }
+            else
+            {
+                lonelySocks.Add(sock);
+            }
+        }
+
+        return pairs;
+    }
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').Select(temp => Convert.ToInt32(temp)).ToList();
+
+        int result = sockMerchant(n, ar);
+
+        Console.WriteLine(result);
+    }
+}
