6443e564cc1405308e019a24f6f243aa062fbd57
Rick-strela <aziz.miner240@gmail.com>
2026-05-24 21:04:54 +0300

    feat: виконано практичну роботу 11 (Drawing Book)

--- src/hackerrank/DrawingBook.cs
+++ src/hackerrank/DrawingBook.cs
@@ -1,8 +1,38 @@
 ﻿using System;
 
-public class Class1
+class Program
 {
-	public Class1()
-	{
-	}
+    static void ShowLog(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine($"[АОП ЛОГ]: {message}");
+        Console.ResetColor();
+    }
+
+    static int PageCount(int n, int p)
+    {
+        int fromFront = p / 2;
+        int fromBack = n / 2 - fromFront;
+        return Math.Min(fromFront, fromBack);
+    }
+
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.WriteLine("Введіть загальну кількість сторінок (n):");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введіть цільову сторінку (p):");
+        int p = Convert.ToInt32(Console.ReadLine());
+
+        ShowLog($"Запит на розрахунок. Книга: {n} стор. Шукаємо: {p} стор.");
+
+        int result = PageCount(n, p);
+
+        ShowLog($"Розрахунок завершено. Результат: {result}");
+
+        Console.WriteLine($"\nКінцева відповідь: {result}");
+        Console.ReadLine();
+    }
 }
